850aa4af8155574f88d0228ac465c42e3ee2f988
yousef <yousefshwaly@gmail.com>
2025-11-10 09:30:40 +0200

    Create Layout for account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,24 +1,11 @@
-import Image from "next/image";
-import bg from "@/public/bg.png";
-
-
 export const metadata = {
   title: "Guest area",
 };
 
 export default function page() {
   return (
-    <div className=" bg-blue-200 p-4">
-      <Image
-        src={bg}
-        alt="Next.js logo"
-        width={0}
-        height={0}
-        placeholder="blur"
-        quality={100}
-
-      />
-      <h1>User Account</h1>
+    <div>
+      <h1 className="text-accent-400  text-2xl">User Account</h1>
     </div>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -25,8 +25,8 @@ export default function RootLayout({ children }) {
       >
         <Header />
 
-        <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        <div className="flex-1 px-8 py-4 grid">
+          <main>{children}</main>
         </div>
       </body>
     </html>
