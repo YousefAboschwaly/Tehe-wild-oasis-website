23becff356cd6b28069e3f52612ab5b22e9516ad
yousef <yousefshwaly@gmail.com>
2025-11-11 13:29:12 +0200

    Solve problem of Next with Image to Improve efficiency of image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ersqmuthcoyjqdgtqytk.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/cabin-images/**',
+      },
+    ],
+  },
+
   reactCompiler: true,
 };
 
