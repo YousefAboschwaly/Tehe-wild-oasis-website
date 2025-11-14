9436f65b6257b744ca84acf4295073a284f53196
yousef <yousefshwaly@gmail.com>
2025-11-14 09:24:11 +0200

    Solve problem of build

--- app/about/page.js
+++ app/about/page.js
@@ -6,7 +6,7 @@ import { getCabins } from "../_lib/data-service";
 export const metadata = {
   title: "About",
 };
-export const revalidate = 3600 * 24;
+export const revalidate = 86400;
 export default async function Page() {
   const cabins = await getCabins();
 
