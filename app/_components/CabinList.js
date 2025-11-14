d843ddafb2cb61ad77d91f828bdf5473104ece72
yousef <yousefshwaly@gmail.com>
2025-11-14 11:30:48 +0200

    Make Concept of Filter

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -14,7 +14,6 @@ export default async function CabinList({filter}) {
   else if (filter === 'small') filteredCabins = cabins.filter(cabin => cabin.maxCapacity <=3)
   else if (filter === 'medium') filteredCabins = cabins.filter(cabin => cabin.maxCapacity >=4 && cabin.maxCapacity <=7)
   else if (filter === 'large') filteredCabins = cabins.filter(cabin => cabin.maxCapacity >=8)  
-console.log(filter,filteredCabins)
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
       {filteredCabins.map((cabin) => (
--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,3 +1,7 @@
+"use client"
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 const filterButtons = [
   { label: `All cabins`, value: "all" },
   { label: `1\u20133 guests`, value: "small" },
@@ -6,10 +10,22 @@ const filterButtons = [
 ];
 
 export default function Filter() {
+const searchParams = useSearchParams()  
+const router = useRouter()
+const pathname = usePathname()
+const activeFilter = searchParams.get('capacity')
+
+function handleFilter(filter){
+const params = new URLSearchParams(searchParams.toString())
+params.set('capacity', filter)
+
+  router.replace(`${pathname}?${params.toString()}`,{scroll:false})
+}
+
   return (
     <div className='border border-primary-700'>
       {filterButtons.map((button) => (
-        <button className={`px-5 py-2 hover:bg-primary-700 cursor-pointer`} key={button.value}>
+        <button onClick={()=>handleFilter(button.value)} className={`px-5 py-2 hover:bg-primary-700 cursor-pointer ${activeFilter === button.value ? 'bg-primary-700 text-primary-100':''}`} key={button.value}>
           {button.label}
         </button>
       ))}
