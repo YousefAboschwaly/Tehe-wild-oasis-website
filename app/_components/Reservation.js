f5942b70a0a2e97fb953c1f828493e1cec9d63d9
yousef <yousefshwaly@gmail.com>
2025-11-16 22:15:19 +0200

    Using auth data in Reservation

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,16 +1,27 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
-export default async function Reservation({cabin}) {
- const [bookedDates , settings] =  await Promise.all([
+export default async function Reservation({ cabin }) {
+  const [bookedDates, settings, session] = await Promise.all([
     getBookedDatesByCabinId(cabin.id),
-    getSettings()
-  ])
+    getSettings(),
+    auth(),
+  ]);
   return (
     <div className="grid grid-cols-2 border  border-primary-800 min-h-[400px] overflow-hidden">
-      <DateSelector cabin={cabin} bookedDates={bookedDates} settings={settings}  />
-      <ReservationForm cabin={cabin}  />
+      <DateSelector
+        cabin={cabin}
+        bookedDates={bookedDates}
+        settings={settings}
+      />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,26 +1,33 @@
-function ReservationForm({cabin}) {
+import Image from "next/image";
+
+function ReservationForm({ cabin, user }) {
   // CHANGE
   const maxCapacity = cabin.maxCapacity;
-
+  console.log(user);
   return (
-    <div className=" scale-[1.01]  flex flex-col  "> {/* Added h-full here */}
+    <div className=" scale-[1.01]  flex flex-col  ">
+      {" "}
+      {/* Added h-full here */}
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
-      
 
-        {/* <div className='flex gap-4 items-center'>
-          <img
-            // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
-            src={user.image}
-            alt={user.name}
-          />
+        <div className="flex gap-4 items-center">
+          <div className="h-8 w-8  relative">
+            <Image
+              // Important to display google profile images
+              referrerPolicy="no-referrer"
+              fill
+              className="object-cover rounded-full"
+              src={user.image}
+              alt={user.name}
+            />
+          </div>
           <p>{user.name}</p>
-        </div> */}
+        </div>
       </div>
-
- <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col h-full"> {/* Added flex-1 and h-full here */}
+      <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col h-full">
+        {" "}
+        {/* Added flex-1 and h-full here */}
         <div className="space-y-4">
           <label htmlFor="numGuests">How many guests?</label>
           <select
@@ -39,7 +46,6 @@ function ReservationForm({cabin}) {
             ))}
           </select>
         </div>
-
         <div className="space-y-4">
           <label htmlFor="observations">
             Anything we should know about your stay?
@@ -51,10 +57,13 @@ function ReservationForm({cabin}) {
             placeholder="Any pets, allergies, special requirements, etc.?"
           />
         </div>
-
-        <div className="mt-auto"> {/* Added mt-auto to push this to bottom */}
+        <div className="mt-auto">
+          {" "}
+          {/* Added mt-auto to push this to bottom */}
           <div className="flex justify-end items-center gap-6">
-            <p className="text-primary-300 text-base">Start by selecting dates</p>
+            <p className="text-primary-300 text-base">
+              Start by selecting dates
+            </p>
 
             <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
               Reserve now
