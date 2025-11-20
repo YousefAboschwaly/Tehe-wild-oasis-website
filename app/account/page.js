07fb9bafa80873f4f3a7b198736c69f0d409c0aa
yousef <yousefshwaly@gmail.com>
2025-11-20 23:59:38 +0200

    fetch userData in account page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function page() {
+export default async function page() {
+  const session = await auth()
   return (
     <div>
-      <h1 className="text-accent-400  text-2xl">User Account</h1>
+      <h1 className="text-accent-400  text-2xl">Welcome {session.user.name}</h1>
     </div>
   );
 }
