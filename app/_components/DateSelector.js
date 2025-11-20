09887f3101ed97a3efbb207fad98afa586f8d151
yousef <yousefshwaly@gmail.com>
2025-11-21 00:12:42 +0200

    handle needed data in DateSelector an disabled the past dates

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,5 +1,5 @@
 "use client";
-import { isWithinInterval } from "date-fns";
+import { differenceInDays, isPast, isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "../_context/ReservationContext";
@@ -14,47 +14,46 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector({cabin , settings, bookedDates}) {
+function DateSelector({ cabin, settings, bookedDates }) {
   // CHANGE
-  const {  range ,setRange , resetRange} = useReservation();
-  const {regularPrice , discount } = cabin
-  const numNights = 23;
-  const cabinPrice = 23;
+  const { range, setRange, resetRange } = useReservation();
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(range.to, range.from);
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
-  const {minBookingLength, maxBookingLength} = settings
+  const { minBookingLength, maxBookingLength } = settings;
 
-console.log(range)
+  console.log(range);
   return (
     <div className="flex-1  gap-2 flex flex-col justify-between w-full h-full ">
-        <DayPicker
+      <DayPicker
         onSelect={setRange}
         selected={range}
-          mode="range"
-          min={minBookingLength + 1}
-          max={maxBookingLength}
-          fromMonth={new Date()}
-          fromDate={new Date()}
-          toYear={new Date().getFullYear() + 5}
-          captionLayout="dropdown"
-          numberOfMonths={2}
-          className="pt-12 place-items-center"
-          classNames={{
-            months: "flex gap-3  ",
-            nav:"hidden",
-            today: "text-accent-500 ",
-            month_caption: " mb-3",
-            dropdowns: " flex justify-center gap-4 cursor-pointer ",
-            day: "hover:rounded-full hover:bg-accent-400 hover:text-primary-900 transition-all",
-            day_button: "h-10 w-10 ",
-            selected: "hover:rounded-none bg-accent-500 text-primary-900",
-            range_start: "rounded-l-full",
-            range_middle: "bg-accent-500 ",
-            range_end: "rounded-r-full",
-          
-          }}
-
-        />
+        mode="range"
+        min={minBookingLength + 1}
+        max={maxBookingLength}
+        fromMonth={new Date()}
+        fromDate={new Date()}
+        toYear={new Date().getFullYear() + 5}
+        captionLayout="dropdown"
+        numberOfMonths={2}
+        disabled={(curDate) => isPast(curDate)}
+        className="pt-12 place-items-center"
+        classNames={{
+          months: "flex gap-3  ",
+          nav: "hidden",
+          today: "text-accent-500 ",
+          month_caption: " mb-3",
+          dropdowns: " flex justify-center gap-4 cursor-pointer ",
+          day: "hover:rounded-full hover:bg-accent-400 hover:text-primary-900 transition-all",
+          day_button: "h-10 w-10 ",
+          selected: "hover:rounded-none bg-accent-500 text-primary-900",
+          range_start: "rounded-l-full",
+          range_middle: "bg-accent-500 ",
+          range_end: "rounded-r-full",
+        }}
+      />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
         <div className="flex items-baseline gap-6">
