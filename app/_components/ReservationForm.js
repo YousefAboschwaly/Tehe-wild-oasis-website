70290630b8b1252f8880c3f84bb15da4cece7370
yousef <yousefshwaly@gmail.com>
2025-11-21 23:52:33 +0200

    reset range after createBooking

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useReservation } from "../_context/ReservationContext";
 import { differenceInDays } from "date-fns";
 import { createReservation } from "../_lib/actions";
 
 function ReservationForm({ cabin, user }) {
-  const { range } = useReservation();
+  const { range, resetRange } = useReservation();
   const { maxCapacity, regularPrice, discount, id } = cabin;
   const startDate = range.from;
   const endDate = range.to;
@@ -20,7 +20,7 @@ function ReservationForm({ cabin, user }) {
     cabinId: id,
   };
 
-  const createReservationWithData = createReservation.bind(null , bookingData)
+  const createReservationWithData = createReservation.bind(null, bookingData);
 
   return (
     <div className=" scale-[1.01]  flex flex-col  ">
@@ -43,7 +43,14 @@ function ReservationForm({ cabin, user }) {
           <p>{user.name}</p>
         </div>
       </div>
-      <form action={createReservationWithData} className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col h-full">
+      <form
+        // action={createReservationWithData}
+        action={async(formData) => {
+         await createReservationWithData(formData);
+          resetRange()
+        }}
+        className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col h-full"
+      >
         {" "}
         {/* Added flex-1 and h-full here */}
         <div className="space-y-4">
